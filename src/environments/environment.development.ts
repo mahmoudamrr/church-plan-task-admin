fd4d5d9dbef2d586893bebf9b40d73b5bafd1797
mahmoudamrr <mahmoud.mansour.amr.mm@gmail.com>
2023-12-25 20:18:56 +0200

    envs

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  application: {
+    name: 'church-plan',
+    angular: 'Angular 17.0.7 Dev',
+    bootstrap: 'Bootstrap 5.3.2',
+    fontawesome: 'Font Awesome 6.5.1',
+  },
+
+  config: {
+    api: true,
+    url: 'http://localhost:3333/v1/',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  application: {
+    name: 'church-plan',
+    angular: 'Angular 17.0.7',
+    bootstrap: 'Bootstrap 5.3.2',
+    fontawesome: 'Font Awesome 6.5.1',
+  },
+
+  config: {
+    /* LOCAL REST API CRUD WITH SQL LITE */
+    api: true,
+    url: 'http://127.0.0.1/',
+  },
+};
