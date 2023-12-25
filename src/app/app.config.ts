ee818de51281e07250bfd510d12fe2d0ed7efe4e
mahmoudamrr <mahmoud.mansour.amr.mm@gmail.com>
2023-12-25 20:16:41 +0200

    init and app config

---
+++ src/app/app.config.ts
@@ -0,0 +1,34 @@
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
+
+import { withInMemoryScrolling } from '@angular/router';
+
+import { provideToastr } from 'ngx-toastr';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    provideAnimations(),
+    provideToastr(),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      })
+    ),
+    provideClientHydration(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
+};
